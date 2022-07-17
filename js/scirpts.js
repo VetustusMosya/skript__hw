617ec190e839a8ae7c2c432174d5f71c20a776e4
Moiseev <moiseevsenya2002@gmail.com>
2022-07-17 14:17:13 +0300

    readme added

--- js/scirpts.js
+++ js/scirpts.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let numberOfFilms = +prompt("Сколько фильмов вы посмотрели?");
+let numberOfFilms;
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -10,36 +10,61 @@ const personalMovieDB = {
   privat: false,
 };
 
-for (let i = 0; i < numberOfFilms; i++) {
-  let filmName = prompt("Один из последних просмотренных фильмов?");
-  let filmGrade = prompt("На сколько оцените его?");
+function start() {
+  numberOfFilms = +prompt("Сколько фильмов вы посмотрели?");
 
-  if (
-    (filmName === "" || filmName.length > 50 || filmName == null) &&
-    (filmGrade === "" || filmGrade.length > 50 || filmGrade == null)
-  ) {
-    alert("Не правильный формат. Попробуйте еще раз.");
-    i--;
-    continue;
-  } else {
-    personalMovieDB.movies[filmName] = filmGrade;
+  while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt("Сколько фильмов вы посмотрели?");
   }
 }
 
-switch (true) {
-  case numberOfFilms > 0 && numberOfFilms <= 10:
-    alert("Мало");
-    break;
-  case numberOfFilms > 10 && numberOfFilms < 30:
-    alert("Норм");
-    break;
-  case numberOfFilms >= 30:
-    alert("Хорош");
-    break;
-  case numberOfFilms == 0:
-  default:
-    alert("error");
-    break;
+start();
+// rememberMyFilms();
+// detectPersonalLevel();
+showMyDB();
+
+function showMyDB() {
+  if (personalMovieDB.privat === false) {
+    console.log(personalMovieDB);
+  } else {
+    alert("Приватнный.");
+  }
+}
+
+function rememberMyFilms() {
+  for (let i = 0; i < numberOfFilms; i++) {
+    let filmName = prompt("Один из последних просмотренных фильмов?");
+    let filmGrade = prompt("На сколько оцените его?");
+
+    if (
+      (filmName === "" || filmName.length > 50 || filmName == null) &&
+      (filmGrade === "" || filmGrade.length > 50 || filmGrade == null)
+    ) {
+      alert("Не правильный формат. Попробуйте еще раз.");
+      i--;
+      continue;
+    } else {
+      personalMovieDB.movies[filmName] = filmGrade;
+    }
+  }
+}
+
+function detectPersonalLevel() {
+  switch (true) {
+    case numberOfFilms > 0 && numberOfFilms <= 10:
+      alert("Мало");
+      break;
+    case numberOfFilms > 10 && numberOfFilms < 30:
+      alert("Норм");
+      break;
+    case numberOfFilms >= 30:
+      alert("Хорош");
+      break;
+    case numberOfFilms == 0:
+    default:
+      alert("error");
+      break;
+  }
 }
 
 console.log(personalMovieDB);
