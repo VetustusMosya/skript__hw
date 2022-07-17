509074ba578bd852f41e0c3bc1736bd0cef1ad3c
Moiseev <moiseevsenya2002@gmail.com>
2022-07-17 14:58:19 +0300

    third done

--- js/scirpts.js
+++ js/scirpts.js
@@ -18,9 +18,12 @@ function start() {
   }
 }
 
-start();
+// start();
 // rememberMyFilms();
+// rememberMyFilmsWhile();
+// rememberMyFilmsDoWhile();
 // detectPersonalLevel();
+writeYourGenres();
 showMyDB();
 
 function showMyDB() {
@@ -28,6 +31,12 @@ function showMyDB() {
     console.log(personalMovieDB);
   } else {
     alert("Приватнный.");
+  }
+}
+
+function writeYourGenres() {
+  for (let i = 1; i <= 3; i++) {
+    personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
   }
 }
 
@@ -67,36 +76,38 @@ function detectPersonalLevel() {
   }
 }
 
-console.log(personalMovieDB);
+function rememberMyFilmsWhile() {
+  while (numberOfFilms > 0) {
+    let filmName = prompt("Один из последних просмотренных фильмов?");
+    let filmGrade = prompt("На сколько оцените его?");
 
-// while (numberOfFilms > 0) {
-//   let filmName = prompt("Один из последних просмотренных фильмов?");
-//   let filmGrade = prompt("На сколько оцените его?");
+    if (
+      (filmName === "" || filmName.length > 50 || filmName == null) &&
+      (filmGrade === "" || filmGrade.length > 50 || filmGrade == null)
+    ) {
+      alert("Не правильный формат. Попробуйте еще раз.");
+      continue;
+    } else {
+      personalMovieDB.movies[filmName] = filmGrade;
+      numberOfFilms--;
+    }
+  }
+}
 
-//   if (
-//     (filmName === "" || filmName.length > 50 || filmName == null) &&
-//     (filmGrade === "" || filmGrade.length > 50 || filmGrade == null)
-//   ) {
-//     alert("Не правильный формат. Попробуйте еще раз.");
-//     continue;
-//   } else {
-//     personalMovieDB.movies[filmName] = filmGrade;
-//     numberOfFilms--;
-//   }
-// }
+function rememberMyFilmsDoWhile() {
+  do {
+    let filmName = prompt("Один из последних просмотренных фильмов?");
+    let filmGrade = prompt("На сколько оцените его?");
 
-// do {
-//   let filmName = prompt("Один из последних просмотренных фильмов?");
-//   let filmGrade = prompt("На сколько оцените его?");
-
-//   if (
-//     (filmName === "" || filmName.length > 50 || filmName == null) &&
-//     (filmGrade === "" || filmGrade.length > 50 || filmGrade == null)
-//   ) {
-//     alert("Не правильный формат. Попробуйте еще раз.");
-//     continue;
-//   } else {
-//     personalMovieDB.movies[filmName] = filmGrade;
-//     numberOfFilms--;
-//   }
-// } while (numberOfFilms > 0);
+    if (
+      (filmName === "" || filmName.length > 50 || filmName == null) &&
+      (filmGrade === "" || filmGrade.length > 50 || filmGrade == null)
+    ) {
+      alert("Не правильный формат. Попробуйте еще раз.");
+      continue;
+    } else {
+      personalMovieDB.movies[filmName] = filmGrade;
+      numberOfFilms--;
+    }
+  } while (numberOfFilms > 0);
+}
