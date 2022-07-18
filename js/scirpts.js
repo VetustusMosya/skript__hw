0bf461356631c07104c772f1e1f9829fe475bb80
Moiseev <moiseevsenya2002@gmail.com>
2022-07-18 16:35:36 +0300

    fourth done

--- js/scirpts.js
+++ js/scirpts.js
@@ -8,73 +8,99 @@ const personalMovieDB = {
   actors: {},
   genres: [],
   privat: false,
+
+  toggleVisibleMyDB: function () {
+    if (this.privat === false) {
+      this.privat = true;
+      console.log("toggled to true");
+    } else {
+      this.privat = false;
+      console.log("toggled to false");
+    }
+  },
+
+  start: function () {
+    numberOfFilms = +prompt("Сколько фильмов вы посмотрели?");
+
+    while (
+      numberOfFilms == "" ||
+      numberOfFilms == null ||
+      isNaN(numberOfFilms)
+    ) {
+      numberOfFilms = +prompt("Сколько фильмов вы посмотрели?");
+    }
+  },
+
+  showMyDB: function () {
+    if (personalMovieDB.privat == false) {
+      console.log(personalMovieDB);
+    } else {
+      console.log("Приватнный.");
+    }
+  },
+
+  writeYourGenres: function () {
+    for (let i = 1; i <= 3; i++) {
+      let anser = prompt(`Ваш любимый жанр под номером ${i}:`);
+      if (anser === "" || anser == null) {
+        alert("Пусто. Введите жанр.");
+        i--;
+        continue;
+      } else {
+        this.genres[i - 1] = anser;
+      }
+    }
+    this.genres.forEach(function (item, i) {
+      console.log(`Любимый жанр ${i + 1} - это ${item}`);
+    });
+  },
+
+  rememberMyFilms: function () {
+    for (let i = 0; i < numberOfFilms; i++) {
+      let filmName = prompt("Один из последних просмотренных фильмов?");
+      let filmGrade = prompt("На сколько оцените его?");
+
+      if (
+        (filmName === "" || filmName.length > 50 || filmName == null) &&
+        (filmGrade === "" || filmGrade.length > 50 || filmGrade == null)
+      ) {
+        alert("Не правильный формат. Попробуйте еще раз.");
+        i--;
+        continue;
+      } else {
+        personalMovieDB.movies[filmName] = filmGrade;
+      }
+    }
+  },
+
+  detectPersonalLevel: function () {
+    switch (true) {
+      case numberOfFilms > 0 && numberOfFilms <= 10:
+        alert("Мало");
+        break;
+      case numberOfFilms > 10 && numberOfFilms < 30:
+        alert("Норм");
+        break;
+      case numberOfFilms >= 30:
+        alert("Хорош");
+        break;
+      case numberOfFilms == 0:
+      default:
+        alert("error");
+        break;
+    }
+  },
 };
 
-function start() {
-  numberOfFilms = +prompt("Сколько фильмов вы посмотрели?");
+// personalMovieDB.start();
+// personalMovieDB.rememberMyFilms();
+// personalMovieDB.detectPersonalLevel();
+// personalMovieDB.writeYourGenres();
+// personalMovieDB.toggleVisibleMyDB();
+personalMovieDB.showMyDB();
 
-  while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
-    numberOfFilms = +prompt("Сколько фильмов вы посмотрели?");
-  }
-}
-
-// start();
-// rememberMyFilms();
 // rememberMyFilmsWhile();
 // rememberMyFilmsDoWhile();
-// detectPersonalLevel();
-writeYourGenres();
-showMyDB();
-
-function showMyDB() {
-  if (personalMovieDB.privat === false) {
-    console.log(personalMovieDB);
-  } else {
-    alert("Приватнный.");
-  }
-}
-
-function writeYourGenres() {
-  for (let i = 1; i <= 3; i++) {
-    personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
-  }
-}
-
-function rememberMyFilms() {
-  for (let i = 0; i < numberOfFilms; i++) {
-    let filmName = prompt("Один из последних просмотренных фильмов?");
-    let filmGrade = prompt("На сколько оцените его?");
-
-    if (
-      (filmName === "" || filmName.length > 50 || filmName == null) &&
-      (filmGrade === "" || filmGrade.length > 50 || filmGrade == null)
-    ) {
-      alert("Не правильный формат. Попробуйте еще раз.");
-      i--;
-      continue;
-    } else {
-      personalMovieDB.movies[filmName] = filmGrade;
-    }
-  }
-}
-
-function detectPersonalLevel() {
-  switch (true) {
-    case numberOfFilms > 0 && numberOfFilms <= 10:
-      alert("Мало");
-      break;
-    case numberOfFilms > 10 && numberOfFilms < 30:
-      alert("Норм");
-      break;
-    case numberOfFilms >= 30:
-      alert("Хорош");
-      break;
-    case numberOfFilms == 0:
-    default:
-      alert("error");
-      break;
-  }
-}
 
 function rememberMyFilmsWhile() {
   while (numberOfFilms > 0) {
