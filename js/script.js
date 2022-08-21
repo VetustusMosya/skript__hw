fa47730c88ff3baef9bbd852b5aa031a6ff42ca0
Moiseev <moiseevsenya2002@gmail.com>
2022-08-21 16:16:49 +0300

    fifth edited

--- js/script.js
+++ js/script.js
@@ -21,35 +21,56 @@ const movieDB = {
     "Ла-ла лэнд",
     "Одержимость",
     "Скотт Пилигрим против...",
+    "Кин-дза-дза",
+    "Я добавлен",
   ],
 };
 
-deleteAllAdv();
+// deleteAllAdv();
 togglePromoGenre();
 togglePromoBg();
-showMovis();
+// showMovis();
+showMovisDinamic();
 
-function deleteAllAdv() {
-  // document.querySelector(".promo__adv").remove();
-  let adv = document.querySelector(".promo__adv");
-  while (adv.firstChild) {
-    adv.firstChild.remove();
-  }
-}
+const adv = document.querySelectorAll(".promo__adv img");
+
+adv.forEach((item) => {
+  item.remove();
+});
+// function deleteAllAdv() {
+//   document.querySelector(".promo__adv").remove();
+//   let adv = document.querySelector(".promo__adv");
+//   while (adv.firstChild) {
+//     adv.firstChild.remove();
+//   }
+// }
 
 function togglePromoGenre() {
-  document.querySelector(".promo__genre").innerHTML = "ДРАМА";
+  document.querySelector(".promo__genre").textContent = "ДРАМА";
 }
 
 function togglePromoBg() {
   document.querySelector(".promo__bg").style.cssText =
-    "background: url(../img/bg.jpg) center center/cover no-repeat;";
+    "background: url(./img/bg.jpg) center center/cover no-repeat;";
 }
 
-function showMovis() {
-  let items = document.querySelectorAll(".promo__interactive-item");
-  console.log(items);
-  for (let i = 0; i < items.length; i++) {
-    items[i].innerHTML = `${i + 1}) ${movieDB.movies.sort()[i]}`;
-  }
+// function showMovis() {
+//   let items = document.querySelectorAll(".promo__interactive-item");
+//   console.log(items);
+//   for (let i = 0; i < movieDB.movies.length; i++) {
+//     items[i].innerHTML = `${i + 1}) ${movieDB.movies.sort()[i]}`;
+//   }
+// }
+
+function showMovisDinamic() {
+  let list = document.querySelector(".promo__interactive-list");
+  let listItem;
+  list.innerHTML = "";
+  movieDB.movies.sort();
+  movieDB.movies.forEach((film, i) => {
+    list.innerHTML += `<li class="promo__interactive-item">
+    ${i + 1}) ${film}
+      <div class="delete"></div>
+    </li>`;
+  });
 }
